ab56cfac1430502a919b5b90559fe1822415cc33
Marcio Martins <marciopmm@gmail.com>
2016-05-07 11:58:27 -0300

    Carga inicial e Início da criação dos modelos.

---
+++ Loja.Model/Estoque.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja.Model
+{
+    public class Estoque
+    {
+        public Produto Produto { get; set; }
+        public int QUantidade { get; set; }
+    }
+}
